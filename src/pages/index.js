9a5f49c6630d9c89a060ade62f33a333bc975fe4
IsaKaillany <isakaillany123@gmail.com>
2023-07-13 21:30:15 -0300

    add index promotions layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from '@/styles/index.module.css'
 import Navbar from '@/components/navbar/navbar'
 import Subtitle from '@/components/tipography/subtitle/subtitle'
 import Container from '@/components/container/container'
@@ -16,14 +17,16 @@ export default function Home() {
             <div>
                 <Navbar />
                 <Container>
-                    <div>
+                    <div className={styles.session}>
                         <Subtitle>Promoções</Subtitle>
-                        <div>
-                            <SaleCard />
+                        <div className={styles.saleContainer}>
+                            <SaleCard /> 
+                            <SaleCard /> 
+                            <SaleCard /> 
                         </div>
                     </div>
 
-                    <div>
+                    <div className={styles.session}>
                         <Subtitle>Outros Jogos</Subtitle>
                     </div>
                 </Container>
